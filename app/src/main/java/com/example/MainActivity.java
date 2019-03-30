85386592fca78801ff1aa3add8dd4c926caa6e2c
zhangfan <zhangfan_ifan@foxmail.com>
2019-03-30 10:54:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
+}
